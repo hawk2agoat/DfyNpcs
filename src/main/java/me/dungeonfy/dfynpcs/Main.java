7c6ebd4d5e4d6609d9bf88f82323296c4afe63ad
BuildTools <unconfigured@null.spigotmc.org>
2025-07-11 13:44:18 -0400

    git push -u origin main

---
+++ src/main/java/me/dungeonfy/dfynpcs/Main.java
@@ -0,0 +1,23 @@
+package me.dungeonfy.dfynpcs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        getLogger().info("Npcs Loaded");
+        getLogger().warning("Could be bad");
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/dungeonfy/dfynpcs/utils/DfyNpc.java
@@ -0,0 +1,55 @@
+package me.dungeonfy.dfynpcs.utils;
+
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Consumer;
+
+public class DfyNpc {
+    private final Location loc;
+    private String name;
+    private EntityType type;
+    private boolean sentient = false;
+    private boolean invulnerable = false;
+    private Consumer<LivingEntity> customizer;
+
+    public DfyNpc(Location loc) {
+        this.loc = loc;
+    }
+
+    public DfyNpc setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public DfyNpc setType(EntityType type) {
+        this.type = type;
+        return this;
+    }
+
+    public DfyNpc setSentience(Boolean sentience) {
+        this.sentient = sentience;
+        return this;
+    }
+
+    public DfyNpc setInvulnerability(Boolean invulnerable) {
+        this.invulnerable = invulnerable;
+        return this;
+    }
+
+    public DfyNpc customizer(Consumer<LivingEntity> consumer) {
+        this.customizer = consumer;
+        return this;
+    }
+
+    public LivingEntity spawn() {
+        LivingEntity entity = (LivingEntity) loc.getWorld().spawnEntity(loc, type);
+        if (name != null) {
+            entity.setCustomName(name);
+            entity.setCustomNameVisible(true);
+        }
+        entity.setAI(sentient);
+        entity.setInvulnerable(invulnerable);
+        return entity;
+    }
+}
